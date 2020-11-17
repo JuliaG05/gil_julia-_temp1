eeaa426d99a8cee3f3dcddac12a5711a97336e4d
JuliaG05 <72765860+JuliaG05@users.noreply.github.com>
2020-11-17 15:09:01 -0500

    Initial files for MakeCode project

---
+++ main.py
@@ -0,0 +1,3 @@
+while True:
+    print( "Temperature (F): " + input.temperature(TemperatureUnit.FAHRENHEIT) + " - Temperature (C): " + input.temperature(TemperatureUnit.CELSIUS) + " Current Room Temperature: 95°F - 35°C ")
+    
---
+++ main.ts
@@ -0,0 +1,3 @@
+while (true) {
+    console.log("Temperature (F): " + input.temperature(TemperatureUnit.Fahrenheit) + " - Temperature (C): " + input.temperature(TemperatureUnit.Celsius) + " Current Room Temperature: 95°F - 35°C ")
+}
