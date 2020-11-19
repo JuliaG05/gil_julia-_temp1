72301f5e5cca22179a5e89106aa56c15fa41a2bc
JuliaG05 <72765860+JuliaG05@users.noreply.github.com>
2020-11-19 13:12:39 -0500

    Update main.ts, main.py

--- main.py
+++ main.py
@@ -1,3 +1,7 @@
+tempf = 84
 while True:
-    print( "Temperature (F): " + input.temperature(TemperatureUnit.FAHRENHEIT) + " - Temperature (C): " + input.temperature(TemperatureUnit.CELSIUS) + " Current Room Temperature: 95°F - 35°C ")
-    
+    print("temperature" +input.temperature(TemperatureUnit.FAHRENHEIT))
+    if input.temperature(TemperatureUnit.FAHRENHEIT) > tempf:
+        light.set_pixel_color (5, light.rgb(255,0,0))
+    else: 
+        light.clear()
--- main.ts
+++ main.ts
@@ -1,3 +1,10 @@
+let tempf = 84
 while (true) {
-    console.log("Temperature (F): " + input.temperature(TemperatureUnit.Fahrenheit) + " - Temperature (C): " + input.temperature(TemperatureUnit.Celsius) + " Current Room Temperature: 95°F - 35°C ")
+    console.log("temperature" + input.temperature(TemperatureUnit.Fahrenheit))
+    if (input.temperature(TemperatureUnit.Fahrenheit) > tempf) {
+        light.setPixelColor(5, light.rgb(255, 0, 0))
+    } else {
+        light.clear()
+    }
+    
 }
